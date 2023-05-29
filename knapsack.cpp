c804b30bec09f50c603322be88f8b2cd011084c9
Jenisish <126805325+Jenisish@users.noreply.github.com>
2023-05-29 15:19:58 +0530

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,62 +1,28 @@
 #include <iostream>
-#include<vector>
 using namespace std;
-
-// Function to find the maximum of two integers
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-// Function to solve 0/1 Knapsack problem using dynamic programming
-int knapsack(int W, int wt[], int val[], int n, bool selected[]) {
-    int i, w;
-    int K[n+1][W+1];
-
-    // Build table K[][] in bottom up manner
-    for (i = 0; i <= n; i++) {
-        for (w = 0; w <= W; w++) {
-            if (i==0 || w==0)
-                K[i][w] = 0;
-            else if (wt[i-1] <= w && val[i-1] + K[i-1][w-wt[i-1]] > K[i-1][w]) {
-                K[i][w] = val[i-1] + K[i-1][w-wt[i-1]];
-                selected[i-1] = true;  // Set selected flag for the current item
-            }
-            else
-                K[i][w] = K[i-1][w];
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[n+1][W+1];
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= W; j++) {
+            if(i==0 || j==0) dp[i][j] = 0;
+            else if(wt[i-1] <= j) dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+            else dp[i][j] = dp[i-1][j];
         }
     }
-    return K[n][W];
+    return dp[n][W];
 }
-
 int main() {
     int n, W;
     cout << "Enter the number of items: ";
     cin >> n;
-
-    int val[n], wt[n];
-    bool selected[n];  // Initialize selected array to false
-    selected[n]=false;
-    cout << "Enter the weight of item " << endl;
-    for (int i = 0; i < n; i++) { 
-        cin >>wt[i];
-   }
-    
-    cout << "Enter the value of item "<<endl;
-    for (int i = 0; i < n; i++) { 
-        cin>> val[i];
-    }
-
-    cout << "Enter the capacity of knapsack: ";
+    cout << "Enter the maximum weight of knapsack: ";
     cin >> W;
-
-    int result = knapsack(W, wt, val, n, selected);
-    cout << "The maximum value that can be obtained is: " << result << endl;
-
-    cout << "Selected items: ";
-    for (int i = 0; i < n; i++) {
-        cout << selected[i] << " ";
+    int wt[n];
+    int val[n];
+    cout << "Enter the weight and value of each item: " << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> wt[i] >> val[i];
     }
-    cout << endl;
-
+    cout << "Maximum value that can be obtained is: " << knapsack(W, wt, val, n) << endl;
     return 0;
 }
